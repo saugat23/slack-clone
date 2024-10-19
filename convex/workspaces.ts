76e67598db67a6416a03f9f07119dc3560ed3059
saugat23 <saugatbhandari8@gmail.com>
2024-10-19 20:43:38 +0545

    sidebar members

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,6 +1,7 @@
 import { v } from 'convex/values';
 import { mutation, query } from './_generated/server';
 import { auth } from './auth';
+import { throws } from 'assert';
 
 export const create = mutation({
   args: {
@@ -21,6 +22,12 @@ export const create = mutation({
       joinCode,
     });
 
+    await ctx.db.insert('members', {
+      userId,
+      workspaceId,
+      role: 'admin',
+    });
+
     return workspaceId;
   },
 });
@@ -28,7 +35,28 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query('workspaces').collect();
+    const userId = await auth.getUserId(ctx);
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query('members')
+      .withIndex('by_user_id', (q) => q.eq('userId', userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+    return workspaces;
   },
 });
 
--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -29,7 +29,7 @@ export default function WorkspaceSwitcher() {
   return (
     <>
       <DropdownMenu>
-        <DropdownMenuTrigger>
+        <DropdownMenuTrigger asChild>
           <Button className='size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl'>
             {workspaceLoading ? (
               <Loader className='animate-spin size-5 shrink-0' />
@@ -38,26 +38,36 @@ export default function WorkspaceSwitcher() {
             )}
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent side='bottom' align='start' className='w-64'>
-          <DropdownMenuItem onClick={() => router.push(`/workspace/${workspaceId}`)} className='cursor-pointer flex-col justify-start items-start capitalize'>
+        <DropdownMenuContent side='bottom' align='start' className='w-64 bg-white p-3 rounded-md flex flex-col mt-2 shadow-md gap-y-2'>
+          <DropdownMenuItem
+            onClick={() => router.push(`/workspace/${workspaceId}`)}
+            className='cursor-pointer flex flex-col justify-start items-start capitalize hover:bg-accent'
+          >
             {workspace?.name}
             <span className='text-xs text-muted-foreground'>
               Active Workspace
             </span>
           </DropdownMenuItem>
-          {filteredWorkspaces?.map((workspace) =>  (
-            <DropdownMenuItem key={workspace._id} className='cursor-pointer capitalize overflow-hidden' onClick={()=> router.push(`/workspace/${workspace._id}`)}>
-            <div className='shrink-0 size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2'>
-            {workspace.name.charAt(0).toUpperCase()}
-            </div>
-            <p className='truncate'>{workspace.name}</p>
+          {filteredWorkspaces?.map((workspace) => (
+            <DropdownMenuItem
+              key={workspace._id}
+              className='cursor-pointer capitalize overflow-hidden flex items-center justify-start hover:bg-accent'
+              onClick={() => router.push(`/workspace/${workspace._id}`)}
+            >
+              <div className='shrink-0 size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2 hover:bg-accent'>
+                {workspace.name.charAt(0).toUpperCase()}
+              </div>
+              <p className='truncate'>{workspace.name}</p>
             </DropdownMenuItem>
           ))}
-          <DropdownMenuItem className='cursor-pointer' onClick={() => setOpen(true)}>
-          <div className='size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-xl rounded-md flex items-center justify-center mr-2'>
-          <Plus />
-          </div>
-          Create a new workspace
+          <DropdownMenuItem
+            className='cursor-pointer flex items-center justify-start hover:bg-accent'
+            onClick={() => setOpen(true)}
+          >
+            <div className='size-9 relative overflow-hidden bg-[#F2F2F2] text-slate-800 font-semibold text-xl rounded-md flex items-center justify-center mr-2'>
+              <Plus />
+            </div>
+            Create a new workspace
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
